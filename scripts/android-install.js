efec274156ba503537db67379ce3b8b722db13e9
shengjin wang <934457719@qq.com>
2023-08-30 11:23:49 +0800

    Update android-install.js
    context.requireCordovaModule to require

--- scripts/android-install.js
+++ scripts/android-install.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-    var path        = context.requireCordovaModule('path'),
-        fs          = context.requireCordovaModule('fs'),
-        shell       = context.requireCordovaModule('shelljs'),
+    var path        = require('path'),
+        fs          = require('fs'),
+        shell       = require('shelljs'),
         projectRoot = context.opts.projectRoot,
         plugins     = context.opts.plugins || [];
 
